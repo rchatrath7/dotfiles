30a27bc447d80c64d87337be61fd73d2b24c1102
Rakesh Chatrath <rakesh.chatrath@shopify.com>
2022-01-14 13:29:11 -0800

    Use absolute path in symlinks

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ln -sfn oh-my-zsh/.oh-my-zsh $HOME/.oh-my-zsh
-ln -sfn tools/fzf/.fzf $HOME/.fzf
-ln -sfn tools/gitz/.gitz $HOME/.gitz
-ln -sfn spacevim/.spacevim $HOME/.spacevim
-ln -sfn spacevim/.space-vim $HOME/.space-vim
-ln -sfn git/.gitconfig $HOME/.gitconfig
+ln -sfn $HOME/oh-my-zsh/.oh-my-zsh $HOME/.oh-my-zsh
+ln -sfn $HOME/tools/fzf/.fzf $HOME/.fzf
+ln -sfn $HOME/tools/gitz/.gitz $HOME/.gitz
+ln -sfn $HOME/spacevim/.spacevim $HOME/.spacevim
+ln -sfn $HOME/spacevim/.space-vim $HOME/.space-vim
+ln -sfn $HOME/git/.gitconfig $HOME/.gitconfig
 
 mkdir -p $HOME/.config/nvim
 
-ln -sfn nvim/init.vim $HOME/.config/nvim/init.vim
-ln -sfn dotrc/.zshrc $HOME/.zshrc
+ln -sfn $HOME/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sfn $HOME/dotrc/.zshrc $HOME/.zshrc
 
 source $HOME/.zshrc
